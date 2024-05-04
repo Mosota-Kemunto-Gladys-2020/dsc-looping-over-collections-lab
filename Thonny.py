edfdc721fdf089a5b7de206a71281bb2090d98b9
Mosota-Kemunto-Gladys-2020 <65958458+Mosota-Kemunto-Gladys-2020@users.noreply.github.com>
2024-05-04 17:41:37 +0300

    Add files via upload

---
+++ Thonny.py
@@ -0,0 +1,17 @@
+car=['model','type','colour']
+for i in car:
+    print(i)
+    
+user= {'Name':'Gladys','Age':35,'nationality': 'Kenya'}
+print(user.get('Name'))
+print(user.values())
+print(user.keys())
+print(user.items())
+print(user.copy())
+user['age']=30
+print(user.items())
+user['sex']='Female'
+print(user.copy())
+user.update({'sex':'female'})
+print(user)
+
